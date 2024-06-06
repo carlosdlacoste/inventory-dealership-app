f3c9ce318dc60712f858a6c213aa06810fcf64d8
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-06-06 11:26:16 -0400

    Update inventoryRoutes with filters

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -4,5 +4,6 @@ const inventoryController = require('../controllers/inventoryController');
 const router = express.Router();
 
 router.get('/', inventoryController.getInventory);
+router.get('/inventory/filter', inventoryController.getFilteredInventory);
 
 module.exports = router;
