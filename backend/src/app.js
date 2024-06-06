ecfb60e1346f20910c5114b834cab4935b919c48
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-06-06 16:48:50 -0400

    Error in history controller method

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const inventoryRoutes = require('./routes/inventoryRoutes');
 const msrpRoutes = require('./routes/msrpRoutes');
+const historyRoutes = require('./routes/historyRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/inventory', inventoryRoutes);
 app.use('/api/msrp', msrpRoutes);
+app.use('/api/history', historyRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
---
+++ backend/src/controllers/historyController.js
@@ -0,0 +1,56 @@
+const msrpController = require('../controllers/msrpController');
+const inventoryController = require('../controllers/inventoryController');
+
+// const buildHistoryLog = async (req, res) => {
+//     try {
+//         // Obtener datos de inventario y MSRP
+//         const inventoryData = await inventoryController.getInventory();
+//         const msrpData = await msrpController.calculateMSRPWithoutFilters();
+
+//         // Calcular inventario total por condición de vehículo
+//         const inventorySummary = calculateInventorySummary(inventoryData);
+
+//         // Construir el historial
+//         const historyLog = {
+//             newInventory: {
+//                 count: inventorySummary.new,
+//                 totalMSRP: msrpData.new.totalMSRP,
+//                 averageMSRP: msrpData.new.averageMSRP
+//             },
+//             usedInventory: {
+//                 count: inventorySummary.used,
+//                 totalMSRP: msrpData.used.totalMSRP,
+//                 averageMSRP: msrpData.used.averageMSRP
+//             },
+//             cpo: {
+//                 count: inventorySummary.cpo,
+//                 totalMSRP: msrpData.cpo.totalMSRP,
+//                 averageMSRP: msrpData.cpo.averageMSRP
+//             }
+//         };
+
+//         // Enviar el historial como respuesta
+//         res.json(historyLog);
+//     } catch (err) {
+//         res.status(500).json({ error: err.message });
+//     }
+// };
+
+// // Función para calcular el inventario total por condición de vehículo
+// const calculateInventorySummary = (inventoryData) => {
+//     const inventorySummary = {
+//         new: 0,
+//         used: 0,
+//         cpo: 0
+//     };
+
+//     // Calcular el inventario total por condición de vehículo
+//     inventoryData.forEach(vehicle => {
+//         const { condition } = vehicle;
+//         inventorySummary[condition]++;
+//     });
+
+//     return inventorySummary;
+// };
+
+// module.exports = { buildHistoryLog };
