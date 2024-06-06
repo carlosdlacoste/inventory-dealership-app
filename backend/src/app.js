1b1e3047dc68cf2f27c654bb615cac00a6a7ea09
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-06-06 15:16:30 -0400

    Update msrpModel and complete msrp calc

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const inventoryRoutes = require('./routes/inventoryRoutes');
+const msrpRoutes = require('./routes/msrpRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/inventory', inventoryRoutes);
+app.use('/api/msrp', msrpRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
--- backend/src/models/msrpModel.js
+++ backend/src/models/msrpModel.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 const csv = require('csv-parser');
 
 const getInventoryData = () => {
     return new Promise((resolve, reject) => {
         const results = [];
-        fs.createReadStream('inventory.csv')
+        const filePath = path.join(__dirname, '../../data/sample-data-v2.csv');
+
+        fs.createReadStream(filePath)
         .pipe(csv())
         .on('data', (data) => {
             results.push(data);
